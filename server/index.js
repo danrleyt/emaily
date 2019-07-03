0e935d80dbcdaee46e37c04dd15cff5fd58fbe2f
Dan Teixeira <danrleyt@gmail.com>
2019-07-02 21:21:08 -0300

    Adding oauth logic with google

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 
