e1ccd0e6721664d8292b5ef8d3fb37afec260972
Dan Teixeira <danrleyt@gmail.com>
2019-11-01 12:28:28 -0300

    Adding materialize and making boilerplate header

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import Header from './Header';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
-  return <div>Hi There!</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header></Header>
+          <Route exact path="/" component={Landing}></Route>
+          <Route exact path="/surveys" component={Dashboard}></Route>
+          <Route path="/surveys/new" component={SurveyNew}></Route>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
---
+++ server/client/src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a className="left brand-logo">Emaily</a>
+          <ul className="right">
+            <li>
+              <a>Login with Google</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Header;
