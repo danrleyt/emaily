1d100d2e32f08e875370e43b6a3000c72c00d935
Dan Teixeira <danrleyt@gmail.com>
2019-11-05 11:48:02 -0300

    Creating login/logout flow on client

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './Header';
+import Landing from './Landing';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
-const Landing = () => <h2>Landing</h2>;
 
-const App = () => {
-  return (
-    <div className="container">
-      <BrowserRouter>
-        <div>
-          <Header></Header>
-          <Route exact path="/" component={Landing}></Route>
-          <Route exact path="/surveys" component={Dashboard}></Route>
-          <Route path="/surveys/new" component={SurveyNew}></Route>
-        </div>
-      </BrowserRouter>
-    </div>
-  );
-};
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
 
-export default App;
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing}></Route>
+            <Route exact path="/surveys" component={Dashboard}></Route>
+            <Route path="/surveys/new" component={SurveyNew}></Route>
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default connect(
+  null,
+  actions
+)(App);
--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,20 +1,40 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class Header extends React.Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  }
+
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
           <a className="left brand-logo">Emaily</a>
-          <ul className="right">
-            <li>
-              <a>Login with Google</a>
-            </li>
-          </ul>
+          <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
     );
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
